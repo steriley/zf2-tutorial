1f75874511256de9a39759420bf32ce006ac05c2
Stephen Riley <stuff@steriley.com>
2012-09-10 14:25:22 +0100

    Database model and view for albums added

---
+++ module/Album/Module.php
@@ -0,0 +1,42 @@
+<?php
+// module/Album/Module.php
+namespace Album;
+
+// Add this import statement:
+use Album\Model\AlbumTable;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+	
+	// Add this method:
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Album\Model\AlbumTable' =>  function($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $table     = new AlbumTable($dbAdapter);
+                    return $table;
+                },
+            ),
+        );
+    }
+}
